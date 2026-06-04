6e833557292eede67d73ba3818dec07011930ab4
walidan-nadilaw <sirsnakeson@gmail.com>
2026-06-04 17:50:44 +0700

    fix : update port : 5001 -> 3000

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -11,7 +11,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: "http://localhost:5001",
+        url: "http://localhost:3000",
         description: "Development server",
       },
     ],
